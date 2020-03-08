7ec30001b9819fbcd00e499e860080379a39acec
Marcelo Arruda Manchester de Queiroga <32495744+marcelomanchester@users.noreply.github.com>
2020-03-08 01:46:28 -0300

    Update importFiles.py

--- importFiles.py
+++ importFiles.py
@@ -1,14 +1,19 @@
-late = open("late.txt", "r")
-lateLines = late.readlines()
-lon = open("lon.txt", "r")
-lonLines = lon.readlines()
-prec = open("lon.txt", "r")
-precLines = prec.readlines()
-newFile = open("final.txt", "w+")
-index = 0
-print("Creating file ...")
-while(index < len(lonLines)-1):
-    if index != 0:
-        newFile.write(str(lateLines[index]).replace("\n", "") + " " + str(lonLines[index]).replace("\n", "") + " " + str(precLines[index]).replace("\n", "") +"\n")
-    index += 1
-print("Finished")
+import datetime
+import os
+
+def format(number):
+    if number < 10:
+        number = '0' + str(number)
+    return str(number)
+
+print('Requesting files ...')
+try:
+    now = datetime.datetime.now()
+    os.system("curl http://ftp.cptec.inpe.br/modelos/io/produtos/MERGE/GPM/HOURLY/MERGE_GPM_EARLY/"+ str(now.year) + format(now.month) + "/MERGE_GPM_early_"+ str(now.year) + format(now.month) + format(now.day-2) +"00.ctl -o MERGE_GPM_early_"+ str(now.year) + format(now.month) + format(now.day-2) +"00.ctl")
+    os.system("curl http://ftp.cptec.inpe.br/modelos/io/produtos/MERGE/GPM/HOURLY/MERGE_GPM_EARLY/"+ str(now.year) + format(now.month) + "/MERGE_GPM_early_"+ str(now.year) + format(now.month) + format(now.day-2) +"00.grib2 -o MERGE_GPM_early_"+ str(now.year) + format(now.month) + format(now.day-2) +"00.grib2")
+    os.system("curl http://ftp.cptec.inpe.br/modelos/io/produtos/MERGE/GPM/HOURLY/MERGE_GPM_EARLY/"+ str(now.year) + format(now.month) + "/MERGE_GPM_early_"+ str(now.year) + format(now.month) + format(now.day-2) +"00.idx -o MERGE_GPM_early_"+ str(now.year) + format(now.month) + format(now.day-2) +"00.idx")
+    print("Finished. Your data is available in this directory!")
+except:
+    print("Error!!!")
+
+
