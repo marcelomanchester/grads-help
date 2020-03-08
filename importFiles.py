26c0281214aa4c82930617d6b992ea546f1cf18e
Marcelo Arruda Manchester de Queiroga <32495744+marcelomanchester@users.noreply.github.com>
2020-03-08 01:29:55 -0300

    Create importFiles.py

---
+++ importFiles.py
@@ -0,0 +1,14 @@
+late = open("late.txt", "r")
+lateLines = late.readlines()
+lon = open("lon.txt", "r")
+lonLines = lon.readlines()
+prec = open("lon.txt", "r")
+precLines = prec.readlines()
+newFile = open("final.txt", "w+")
+index = 0
+print("Creating file ...")
+while(index < len(lonLines)-1):
+    if index != 0:
+        newFile.write(str(lateLines[index]).replace("\n", "") + " " + str(lonLines[index]).replace("\n", "") + " " + str(precLines[index]).replace("\n", "") +"\n")
+    index += 1
+print("Finished")
